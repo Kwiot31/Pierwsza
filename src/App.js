c682cec342113744c6a3b83b830b2ea1e2d71e17
Kwiot31 <kubakwiat31@gmail.com>
2024-07-29 11:48:32 +0200

    pierwsza zmiana

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./komponenty/Button";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        
+      
+        <Button/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/komponenty/Button.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './Button.css';
+
+const Button = ({ text, style }) => {
+    const [message, setMessage] = useState('');
+
+    const handleClick = () => {
+        setMessage('Super, udało się!');
+    };
+
+    return (
+        <div class="center-text">
+            <button className="custom-button" onClick={handleClick} style={style}>
+                {text} Kliknij mnie ale co dalej
+            </button>
+            {message && <p>{message}</p>}
+        </div>
+    );
+};
+
+export default Button;
